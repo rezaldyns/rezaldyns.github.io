0eea3de6cd7adb46f9223a0998dbe85f11609d19
rezaldyns <rezaldyns@icloud.com>
2018-03-08 10:47:38 +0700

    first upload

---
+++ js/main.js
@@ -0,0 +1,52 @@
+window.sr = ScrollReveal({ reset: false });
+
+sr.reveal(document.getElementById('foto'), { duration: 1000 });
+sr.reveal(document.getElementById('hello'), { duration: 1100 });
+sr.reveal(document.getElementById('personal'), { duration: 1200 });
+sr.reveal(document.getElementById('minimalpic'), 
+{ 
+    duration: 1200,
+    origin: 'right'
+
+});
+sr.reveal(document.getElementById('cryptopic'), 
+{ 
+    duration: 1200,
+    origin: 'left'
+
+});
+
+//cryptoweb
+
+sr.reveal(document.getElementById('gambarcrypto'), { duration: 1000 });
+sr.reveal(document.getElementById('secondimg'), 
+{ 
+    duration: 1000,
+    origin: 'left'
+
+});
+sr.reveal(document.getElementById('thirdimg'), 
+{ 
+    duration: 1000,
+    origin: 'right'
+
+});
+
+sr.reveal(document.getElementById('img-1'), 
+{ 
+    duration: 1100,
+    origin: 'left'
+
+});
+sr.reveal(document.getElementById('img-2'), 
+{ 
+    duration: 1300,
+    origin: 'bottom'
+
+});
+sr.reveal(document.getElementById('img-3'), 
+{ 
+    duration: 1200,
+    origin: 'right'
+
+});
